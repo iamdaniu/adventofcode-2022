4eec107d826f20c6e4d9b16e3f6204f5ee853e6f
Jörn Schumacher <joernspam@gmx.de>
2022-12-15 11:18:08 +0100

    refactoring Grid to use Map internally, allowing negative values

--- src/de/joern/Grid.java
+++ src/de/joern/Grid.java
@@ -1,28 +1,27 @@
 package de.joern;
 
-import de.joern.Coordinate;
 import lombok.Getter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Grid<T> {
-    @SuppressWarnings("unchecked")
-    private final T[][] grid = (T[][]) new Object[1_000][1_000];
+    private final Map<Coordinate, T> contentMap = new HashMap<>();
+    private final T defaultValue;
+
     private int highestY = Integer.MAX_VALUE;
     private int leftmostX = Integer.MAX_VALUE, rightmostX;
     @Getter
     private int lowestY;
 
     public Grid(T defaultValue) {
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                grid[i][j] = defaultValue;
-            }
-        }
+        this.defaultValue = defaultValue;
     }
 
     public void setContent(Coordinate coord, T content) {
+        contentMap.put(coord, content);
         var x = coord.x();
         var y = coord.y();
-        grid[x][y] = content;
         lowestY = Math.max(lowestY, y);
         highestY = Math.min(highestY, y);
         leftmostX = Math.min(x, leftmostX);
@@ -33,13 +32,13 @@ public class Grid<T> {
         System.out.printf("top left: (%d/%d), bottom right: (%d/%d)%n", leftmostX, highestY, rightmostX, lowestY);
         for (int y = highestY; y < lowestY + 1; y++) {
             for (int x = leftmostX; x < rightmostX + 1; x++) {
-                System.out.print(grid[x][y]);
+                System.out.print(contentsAt(x, y));
             }
             System.out.println();
         }
     }
 
     public T contentsAt(int x, int y) {
-        return grid[x][y];
+        return contentMap.getOrDefault(new Coordinate(x, y), defaultValue);
     }
 }
